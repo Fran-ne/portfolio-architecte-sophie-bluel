f684d8cccd329dcf50e0afe4822e26d11ade71eb
Fran-ne <francesca.lucchetti@orange.fr>
2026-01-21 13:50:36 +0100

    Ajout des filtres par categories

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,34 +1,100 @@
 //Fichier JS
 
 console.log("script java marche");
+
 const projets = document.querySelector(".gallery");
 
-//API appel
-fetch('http://localhost:5678/api/works')
-    .then(function (reponseserveur) {
-        return reponseserveur.json();
+//Filtres
+const filtersContainer = document.querySelector(".filters");
+let allWorks = [];
+
+//Mettre galerie vide + afficher projets
+function afficherProjets(listeProjets) {
+
+    //Mettre galerie vide
+    projets.innerHTML = "";
+
+    //Boucle projets
+    for (let projet of listeProjets) {
+
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        const caption = document.createElement("figcaption");
+
+        image.src = projet.imageUrl;
+        image.alt = projet.title;
+        caption.textContent = projet.title;
+
+        figure.appendChild(image);
+        figure.appendChild(caption);
+
+        //ajout galerie ?
+        projets.appendChild(figure);
+    }
+}
+
+//Fonction boutons
+function creerBoutons(categories) {
+
+    filtersContainer.innerHTML = ""; //vide
+
+    // Boutons
+    const bouton = document.createElement("button");
+    bouton.textContent = "Tous";
+    bouton.classList.add("active");
+    filtersContainer.appendChild(bouton);
+
+    //fonction event
+    bouton.addEventListener("click", function () {
+        document.querySelectorAll(".filters button").forEach(function (bouton) {
+            bouton.classList.remove("active"); //enleve classe active(un bouton)
+        });
+
+        bouton.classList.add("active");
+        afficherProjets(allWorks); //afficher projets
+    });
+
+    //Categories Boutons
+    for (let categorie of categories) {
+
+        const bouton = document.createElement("button");
+        bouton.textContent = categorie.name;
+        filtersContainer.appendChild(bouton);
+
+        //event bouton
+        bouton.addEventListener("click", function () {
+            console.log("btn click :", bouton.textContent);
+
+            document.querySelectorAll(".filters button").forEach(function (bouton) {
+                bouton.classList.remove("active");
+            });
+
+            bouton.classList.add("active");
+
+            const projetsFiltres = allWorks.filter(function (projet) {
+                return projet.categoryId === categorie.id;
+            });
+
+            afficherProjets(projetsFiltres);
+        });
+    }
+}
+
+// fetch les projets api
+fetch("http://localhost:5678/api/works")
+    .then(function (reponse) {
+        return reponse.json();
     })
-    .then(function (data) {
-        console.log("projets retour:", data);
+    .then(function (works) {
+        allWorks = works;
+        afficherProjets(allWorks);
+    });
 
-        //Mettre galerie vide
-        projets.innerHTML = "";
-
-        //Boucle projets
-        for (let projet of data) {
-
-            const figure = document.createElement("figure");
-            const image = document.createElement("img")
-            const caption = document.createElement("figcaption");
-
-            image.src = projet.imageUrl;
-            image.alt = projet.title;
-            caption.textContent = projet.title;
-
-            figure.appendChild(image);
-            figure.appendChild(caption);
-
-            //ajout galerie ?
-            projets.appendChild(figure);
-        }
+//fetch les categories
+fetch("http://localhost:5678/api/categories")
+    .then(function (reponse) {
+        return reponse.json();
+    })
+    .then(function (categories) {
+        creerBoutons(categories);
     });
